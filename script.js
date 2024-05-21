5e12f37cd2e620a33c69323db84561ab70cd610c
stephaniee3e <158521537+stephaniee3e@users.noreply.github.com>
2024-05-21 21:09:36 +0000

    done

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ let button1=document.querySelector(".option-one");
 let button2=document.querySelector(".option-two");
 let option1Screen=document.querySelector(".locker")
 let option2screen=document.querySelector(".home")
-let lastscreen=document.querySelector(".first-class") 
+let button3=document.querySelector(".option1-1")
+let button4=document.querySelector(".option1-2")
+let option3screen=document.querySelector(".first-class")
+let lastscreen=document.querySelector("outside") 
 
 
 
 
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
-
-
+button1.addEventListener("click", function() {
+  option1Screen.style.display = "block"
+  storyOpening.style.display="none"
+});
+button2.addEventListener("click",function(){
+  option2screen.style.display = "block"
+  storyOpening.style.display = "none"
+})
 // INSERT_VARIABLE.addEventListener('click', function(){
 
 // });
